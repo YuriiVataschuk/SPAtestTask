1e478393a0e4be27015230a1efbd56a0e56e470a
yura.vataschuk <yura.vataschuk@gmail.com>
2023-08-22 22:06:08 +0200

    Added db and Comment model

--- SPAtestTask/settings.py
+++ SPAtestTask/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,9 +78,9 @@ WSGI_APPLICATION = "SPAtestTask.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    user_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
